62475230ad3e9d35f01aa806bb472a617b2aa826
codewithSUKU <72142656+codewithSUKU@users.noreply.github.com>
2020-10-01 11:55:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+from pydub import AudioSegment
+import gtts
+
+
+
+def textTospeech(text,filename):
+    pass
+
+def mergeAudios(audios):
+    pass
+
+def generateSkeleton():
+    pass
+
+def generateAnnouncement(filename):
+    pass
+
+
+if __name__ == "__main__":
+    print("Generating Skeleton...")
+    generateSkeleton()
+    
